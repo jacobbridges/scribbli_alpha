c4741c8ff446d46c76210aea15ead1fff42a9e89
Jacob Bridges <me@jacobbridges.dev>
2024-06-27 13:29:47 -0500

    create new middleware for permission errors
    Any PermissionDenied errors will result in a 404 rather than a 403. Extra security through obscurity.

--- scribbli_web/settings.py
+++ scribbli_web/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'scribbli.middleware.handle_exceptions.HandleExceptionMiddleware',
 ]
 
 ROOT_URLCONF = 'scribbli_web.urls'
